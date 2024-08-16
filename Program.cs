bea49eca3c49c5ac27e1f10b8d21220845cac22b
KaiCoconut <58313788+Kailesen@users.noreply.github.com>
2024-08-16 15:45:33 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hiiiii Guys! Can you see this?")
-         Console.WriteLine("AHHHHHH!!!");
+        Console.WriteLine("AHHHHHH!!!");
+        Console.WriteLine("we're so julia");
+        Console.WriteLine("ah AHH ah AH ah");
         Conosle.ReadLine();
         Console.ReadKey();
     }
