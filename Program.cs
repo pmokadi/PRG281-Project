99f1de41257c94ea9b6b062b9e560fd6546743ff
MercenaryTao <sirlvaughan7646@gmail.com>
2024-08-16 15:52:17 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.WriteLine("we're so julia");
         Console.WriteLine("ah AHH ah AH ah");
         Console.WriteLine("What does you're so Julia mean?")
-        Console.WriteLine("julia is a lifestyle. julia is a mindset. julia is BRAT");
+        Console.WriteLine("julia is a lifestyle. julia is a mindset. julia is BRAT"); //only saw this now
         Console.ReadKey();
     }
 }
