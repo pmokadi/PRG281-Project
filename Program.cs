715f9066ab8fe8f9153e5c5d8770d4cfaf0b7908
MercenaryTao <sirlvaughan7646@gmail.com>
2024-08-16 15:46:30 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ class Program
         Console.WriteLine("AHHHHHH!!!");
         Console.WriteLine("we're so julia");
         Console.WriteLine("ah AHH ah AH ah");
+        Console.WriteLine("What does you're so Julia mean?")
         Conosle.ReadLine();
         Console.ReadKey();
     }
